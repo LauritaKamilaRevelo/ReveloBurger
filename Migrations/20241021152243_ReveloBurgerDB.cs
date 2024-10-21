0343a360bff200376c66b918b1b55dd881469a75
LauritaKamilaRevelo <laurita.revelo@udla.edu.ec>
2024-10-21 10:36:03 -0500

    migration

---
+++ Migrations/20241021152243_ReveloBurgerDB.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReveloBurger.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReveloBurgerDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Burger",
+                columns: table => new
+                {
+                    BurgerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WithCheese = table.Column<bool>(type: "bit", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Burger", x => x.BurgerId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Burger");
+        }
+    }
+}
