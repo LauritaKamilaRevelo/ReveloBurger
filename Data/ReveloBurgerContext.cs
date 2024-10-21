9f496fe593d15fecb6033964dfe460634dd99636
LauritaKamilaRevelo <laurita.revelo@udla.edu.ec>
2024-10-21 10:19:32 -0500

    Agregar archivos de proyecto.

---
+++ Data/ReveloBurgerContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ReveloBurger.Models;
+
+    public class ReveloBurgerContext : DbContext
+    {
+        public ReveloBurgerContext (DbContextOptions<ReveloBurgerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ReveloBurger.Models.Burger> Burger { get; set; } = default!;
+    }
---
+++ Models/Burger.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReveloBurger.Models
+{
+    public class Burger
+    {
+        public int BurgerId { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public bool WithCheese { get; set; }
+        
+        [Range(0.1,99.99)]
+        public decimal Price { get; set; }
+    }
+}
